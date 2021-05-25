54cffddd740a265e789f73e79d5b227d7e12cbb5
anantadas <ananta.das@learnwithinfinity.com>
2021-05-25 21:19:52 +0530

    Outputs fixed

--- modules/storage-bucket/outputs.tf
+++ modules/storage-bucket/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_self_link" {
-    value = google_storage_bucket.bucket-01.self_link
+    value = google_storage_bucket.storage-bucket.self_link
 }
 output "bucket_url" {
-    value = google_storage_bucket.bucket-01.url
+    value = google_storage_bucket.storage-bucket.url
 }
 output "bucket_name" {
-    value = google_storage_bucket.bucket-01.name
+    value = google_storage_bucket.storage-bucket.name
 }
